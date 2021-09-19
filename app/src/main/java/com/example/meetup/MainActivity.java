793e7b27f13844c8a2d9a0d98bc7db9246338bb9
Pratham Pote <prathampote@gmail.com>
2021-09-19 13:22:17 +0530

    gradle included

--- app/src/main/java/com/example/meetup/MainActivity.java
+++ app/src/main/java/com/example/meetup/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     Button Login, Register;
     ImageView Logo, Gradient;
     float ytranslate= 1600;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +20,11 @@ public class MainActivity extends AppCompatActivity {
         Login = findViewById(R.id.LoginBtn);
         Register = findViewById(R.id.registerBtn);
         Logo =findViewById(R.id.logo);
-        Gradient = findViewById(R.id.gradient);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Gradient.setTranslationY(300);
-                Logo.setTranslationY(300);
-                Gradient.setAlpha(ytranslate);
-                Logo.setAlpha(ytranslate);
-                Gradient.animate().translationY(0).alpha(1).setDuration(2000).setStartDelay(100).start();
-                Logo.animate().translationY(0).alpha(1).setDuration(2000).setStartDelay(100).start();
+                Logo.animate().translationY(-1000).alpha(1).setDuration(1000).setStartDelay(100).start();
+                Login.animate().translationY(-2000).alpha(1).setDuration(1500).setStartDelay(100).start();
             }
         });
 
