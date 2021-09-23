f4329a81ae31a1973ab8db715e038d302b2cb39b
Pratham Pote <prathampote@gmail.com>
2021-09-24 00:04:00 +0530

    Login and Register Complete 2

---
+++ app/src/main/java/com/example/meetup/User.java
@@ -0,0 +1,14 @@
+package com.example.meetup;
+
+public class User
+{
+    public String Name, Emailid;
+
+    public User() {
+    }
+
+    public User(String name, String emailid) {
+        Name = name;
+        Emailid = emailid;
+    }
+}
